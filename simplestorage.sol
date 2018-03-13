f2c1d612cc596cbe4d7218a88aa2898488af28a4
Jaehyun Cho <looksgoood12@gmail.com>
2018-03-13 23:27:46 +0900

    Add simple example for dapp

---
+++ simplestorage.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.0; 
+
+contract SimpleStorage {
+
+    uint public storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public constant returns (uint) {
+        return storedData;
+    }
+
+}
